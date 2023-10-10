ea1ddc52fe0c440b4777ec7eea25cc83b90e69a1
Chang Ju Kim <kimc13@rpi.edu>
2023-10-10 16:24:48 -0400

    Time: 0 ms (100%), Space: 44.7 MB (8.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,16 +2,18 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[] {-1, -1};
 
+        // Edge cases
         if (nums.length == 0) return result;
         if (nums.length == 1) {
             if (nums[0] != target) return result;
             return new int[] {0, 0};
         }
-        
+
+
         if (nums[0] == target) {
             result[0] = 0;
         } else {
-            // Binary search for target - .5
+            // Binary search for target - 0.5
             int leftIndex = binarySearch(nums, target - 0.5) + 1;
             if (nums[leftIndex] != target) {
                 // Did not find target
@@ -23,6 +25,7 @@ class Solution {
         if (nums[nums.length-1] == target) {
             result[1] = nums.length-1;
         } else {
+            // Binary search for target + 0.5
            result[1] = binarySearch(nums, target + 0.5);
         }
         return result;
